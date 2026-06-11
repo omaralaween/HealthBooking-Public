a1ee0cecf3f3545758646bd6b666bbf77a4b8d32
Omar Alaween <120013591+omaralaween@users.noreply.github.com>
2026-06-12 01:29:03 +0300

    Update API endpoint for fetching appointment slots

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   mounted() {
-    fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/slots")
+    fetch("https://lrpf1ybopg.execute-api.us-east-1.amazonaws.com/prod/slots")
       .then(res => res.json())
       .then(data => {
         const parsed = JSON.parse(data.body);
